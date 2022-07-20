b91427eaf7b058f9aefc01d70351d36a0b4cd6c1
Andres Miranda <andres.miranda.dev@gmail.com>
2022-07-21 00:27:20 +0900

    commiting a bit more progress, still not finished, just saving progress

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const library = [];
+const currentLibrary = [];
 
 function Book (title, author, pages, status){
     this.title = title;
@@ -7,6 +7,36 @@ function Book (title, author, pages, status){
     this.status = status;
 }
 
+const submitButton = document.querySelector("#submit-button")
+
+submitButton.addEventListener("click", function(){
+    const titleInput = document.querySelector("#title").value;
+    const authorInput = document.querySelector("#author").value;
+    const pagesInput = document.querySelector("#pages").value;
+    const statusInput = document.querySelector("#status").value;
+
+    const newBook = new Book(titleInput, authorInput, pagesInput, statusInput);
+    
+    currentLibrary.push(newBook);
+    
+    document.querySelector("#title").value = "";
+    document.querySelector("#author").value = "";
+    document.querySelector("#pages").value = "";
+    document.querySelector("#status").value = "";
+    
+    console.log(currentLibrary)
+    updateLibrary(currentLibrary)
+})
+
+
+function updateLibrary(library){
+    let booksGrid = document.querySelector(".books-grid");
+    library.map((book)=>{
+        let bookContainer = document.createElement("div");
+        bookContainer.innerText = "Title: " + book["title"]+ "\n" + "Author: "+book["author"]+"\n" + "Pages: " + book["pages"] + "\n" + "Status: " + book["status"];
+        return booksGrid.appendChild(bookContainer).className="book";           
+    })
+}
 
 
 
