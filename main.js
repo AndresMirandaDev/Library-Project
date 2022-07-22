bcdbbe37bca6ef75b805675332f0227981050187
Andres Miranda <andres.miranda.dev@gmail.com>
2022-07-23 00:22:02 +0900

    adding the final touches, may come back later to fix this app

--- main.js
+++ main.js
@@ -36,16 +36,30 @@ function addBook(){
      
     const booksGrid = document.querySelector(".books-grid")
     const bookContainer = document.createElement("div");
+    const titleContainer = document.createElement("div");
+    const authorContainer = document.createElement("div");
+    const pagesContainer = document.createElement("div");
+    const statusContainer = document.createElement("div");
 
-    bookContainer.innerText = "Title: " + currentLibrary[currentLibrary.length-1]["title"]+ "\n" + "Author: "+currentLibrary[currentLibrary.length-1]["author"]+"\n" + "Pages: " + currentLibrary[currentLibrary.length-1]["pages"] + "\n" + "Status: " + currentLibrary[currentLibrary.length-1]["status"];
+    titleContainer.innerText = currentLibrary[currentLibrary.length-1]["title"].toUpperCase();
+    authorContainer.innerText = currentLibrary[currentLibrary.length-1]["author"].toUpperCase();
+    pagesContainer.innerText =  currentLibrary[currentLibrary.length-1]["pages"]+ " PAGES";
+    statusContainer.innerText = currentLibrary[currentLibrary.length-1]["status"].toUpperCase();
+   
+    bookContainer.appendChild(titleContainer).className = "book-info";
+    bookContainer.appendChild(authorContainer).className = "book-info";
+    bookContainer.appendChild(pagesContainer).className = "book-info";
+    bookContainer.appendChild(statusContainer).className = "book-info";
+
     const deleteButton = document.createElement("div");
     deleteButton.innerText = "Delete"
         
     const statusButton = document.createElement("div");
     statusButton.innerText = "Change Status"
-
-    bookContainer.appendChild(deleteButton).className = "delete-button";
+    
     bookContainer.appendChild(statusButton).className = "change-status";
+    bookContainer.appendChild(deleteButton).className = "delete-button";
+    
 
     bookContainer.setAttribute("id", currentLibrary[currentLibrary.length-1]["title"])
     booksGrid.appendChild(bookContainer).className = "book";
@@ -58,6 +72,13 @@ const statusButton = document.querySelector(".change-status");
 document.addEventListener("click", function(e){
     if(e.target.className === "delete-button"){
         e.target.parentElement.remove();
+    }else if(e.target.className === "change-status"){
+        const status = e.target.previousElementSibling;
+        if(status.innerText === "READ"){
+            status.innerText = "NOT READ YET"
+        }else if(status.innerText === "NOT READ YET"){
+            status.innerText = "READ"
+        }
     }
 })
 
